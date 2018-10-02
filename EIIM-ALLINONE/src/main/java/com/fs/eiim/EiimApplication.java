de3839df270a98fec2ecd8e42ddee1e9db9a93bf
John Peng <josh_73_9@hotmail.com>
2018-10-02 10:47:14 +0800

    fix(EIIM): 添加了服务器运行和终端运行方式的支持。

--- EIIM-ALLINONE/src/main/java/com/fs/eiim/EiimApplication.java
+++ EIIM-ALLINONE/src/main/java/com/fs/eiim/EiimApplication.java
@@ -11,13 +11,15 @@ public class EiimApplication {
 
     public static void main(String[] args) throws Exception {
         context = new AnnotationConfigApplicationContext(EiimAppConfig.class);
-        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
-        while (true) {
-            System.out.println("Enter quit command to stop the server: [QUIT | quit | EXIT | exit]");
-            String line = reader.readLine();
-            if (line.equalsIgnoreCase("exit") || line.equalsIgnoreCase("quit")) {
-                stopServer();
-                break;
+        if (args.length == 1 && args[0].equalsIgnoreCase("console")) {
+            BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+            while (true) {
+                System.out.println("Enter quit command to stop the server: [QUIT | quit | EXIT | exit]");
+                String line = reader.readLine();
+                if (line.equalsIgnoreCase("exit") || line.equalsIgnoreCase("quit")) {
+                    stopServer();
+                    break;
+                }
             }
         }
     }
