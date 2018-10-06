cab267bffec1ea4dcb2ca8f6ee7f77fb7f03ad38
John Peng <josh_73_9@hotmail.com>
2018-10-06 21:48:40 +0800

    feat(EIIM): 添加了GraphQL的初步验证。

--- EIIM-SERVICE/src/main/java/com/fs/eiim/config/EiimServiceConfig.java
+++ EIIM-SERVICE/src/main/java/com/fs/eiim/config/EiimServiceConfig.java
@@ -2,13 +2,18 @@ package com.fs.eiim.config;
 
 import com.fs.eiim.restful.*;
 import com.fs.eiim.task.EiimInitializeTask;
+import org.mx.service.rest.graphql.GraphQLTypeExecution;
+import org.mx.service.rest.graphql.GraphQLUtils;
 import org.mx.service.server.config.ServerConfig;
 import org.mx.spring.config.SpringConfig;
 import org.mx.spring.task.TaskFactory;
+import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Import;
 
+import java.io.InputStream;
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
@@ -25,6 +30,7 @@ import java.util.List;
 @ComponentScan({
         "com.fs.eiim.service.impl",
         "com.fs.eiim.task",
+        "com.fs.eiim.graphql",
         "com.fs.eiim.restful"
 })
 public class EiimServiceConfig {
@@ -35,9 +41,21 @@ public class EiimServiceConfig {
         return taskFactory;
     }
 
+    @Bean("graphQLUtils")
+    public GraphQLUtils eiimGraphQLUtils(ApplicationContext context) {
+        GraphQLUtils graphQLUtils = new GraphQLUtils();
+        InputStream in = EiimServiceConfig.class.getResourceAsStream("/eiim.graphqls");
+        List<GraphQLTypeExecution> typeExecutions = new ArrayList<>(
+                context.getBeansOfType(GraphQLTypeExecution.class).values()
+        );
+        graphQLUtils.init(in, typeExecutions);
+        return graphQLUtils;
+    }
+
     @Bean("restfulClassesEiim")
     public List<Class<?>> restfulClassesEiim() {
         return Arrays.asList(PersonServiceResource.class, AccountServiceResource.class, ChatRoomServiceResource.class,
-                OrgServiceResource.class, BaseDataServiceResource.class, FileTransformResource.class);
+                OrgServiceResource.class, BaseDataServiceResource.class, FileTransformResource.class,
+                GraphQLResource.class);
     }
 }
